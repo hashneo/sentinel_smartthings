32a2df5104f45fa13c4d228a032a63a7aefea62c
Steven Taylor <steven.taylor@me.com>
2020-10-09 10:54:37 -0700

    update

--- devicetypes/hashneo/sensor-motion.src/sensor-motion.groovy
+++ devicetypes/hashneo/sensor-motion.src/sensor-motion.groovy
@@ -13,44 +13,44 @@
  *  for the specific language governing permissions and limitations under the License.
  */
 metadata {
-	definition (name: "Sentinel Motion Sensor", namespace: "hashneo", author: "Steven Taylor", cstHandler: true) {
-		capability "Battery"
-		capability "Motion Sensor"
+    definition (name: "Sentinel Motion Sensor", namespace: "hashneo", author: "Steven Taylor", cstHandler: true) {
+        capability "Battery"
+        capability "Motion Sensor"
         capability "Sensor"
 
-    	command "zone"
-	}
+        command "zone"
+    }
 
 
-	simulator {
-		// TODO: define status and reply messages here
-	}
+    simulator {
+        // TODO: define status and reply messages here
+    }
 
-  	tiles(scale: 2) {
-    	multiAttributeTile(name:"zone", type: "generic", width: 6, height: 4){
-      		tileAttribute ("device.motion", key: "PRIMARY_CONTROL") {
-        		attributeState "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
-        		attributeState "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#53a7c0"
-        		attributeState "alarm", label:'ALARM', icon:"st.motion.motion.active", backgroundColor:"#ff0000"
-      		}
-    	}
+    tiles(scale: 2) {
+        multiAttributeTile(name:"zone", type: "generic", width: 6, height: 4){
+            tileAttribute ("device.motion", key: "PRIMARY_CONTROL") {
+                attributeState "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
+                attributeState "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#53a7c0"
+                attributeState "alarm", label:'ALARM', icon:"st.motion.motion.active", backgroundColor:"#ff0000"
+            }
+        }
 
         main "zone"
 
-    	details(["zone"])
-	}
+        details(["zone"])
+    }
 }
 
 // parse events into attributes
 def parse(String description) {
-	log.debug "Parsing '${description}'"
-	// TODO: handle 'battery' attribute
-	// TODO: handle 'motion' attribute
+    log.debug "Parsing '${description}'"
+    // TODO: handle 'battery' attribute
+    // TODO: handle 'motion' attribute
 
 }
 
 def zone(String state) {
-	// need to convert open to active and closed to inactive
+    // need to convert open to active and closed to inactive
     def eventMap = [
         'closed':"inactive",
         'open':"active",
@@ -65,7 +65,7 @@ def zone(String state) {
         'alarm':"Alarm Triggered"
     ]
 
-	def desc = descMap."${state}"
+    def desc = descMap."${state}"
 
-  	sendEvent (name: "motion", value: "${newState}", descriptionText: "${desc}")
+    sendEvent (name: "motion", value: "${newState}", descriptionText: "${desc}")
 }
