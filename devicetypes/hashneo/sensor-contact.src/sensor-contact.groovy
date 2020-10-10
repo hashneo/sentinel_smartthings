08b2e6094199074233ab6fe0d37d339cff63cff0
Steven Taylor <steven.taylor@me.com>
2020-10-10 09:32:13 -0700

    new device

--- devicetypes/hashneo/sensor-contact.src/sensor-contact.groovy
+++ devicetypes/hashneo/sensor-contact.src/sensor-contact.groovy
@@ -1,5 +1,5 @@
 /**
- *  sensor-motion
+ *  sensor-contact
  *
  *  Copyright 2020 Steven Taylor
  *
@@ -13,7 +13,7 @@
  *  for the specific language governing permissions and limitations under the License.
  */
 metadata {
-    definition (name: "sensor-motion", namespace: "hashneo", author: "Steven Taylor", cstHandler: true) {
+    definition (name: "sensor-contact", namespace: "hashneo", author: "Steven Taylor", cstHandler: true) {
         capability "Battery"
         capability "Motion Sensor"
         capability "Sensor"
@@ -27,9 +27,9 @@ metadata {
 
     tiles(scale: 2) {
         multiAttributeTile(name:"zone", type: "generic", width: 6, height: 4){
-            tileAttribute ("device.motion", key: "PRIMARY_CONTROL") {
-                attributeState "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
-                attributeState "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#53a7c0"
+            tileAttribute ("device.contact", key: "PRIMARY_CONTROL") {
+                attributeState "inactive", label:'no contact', icon:"st.contact.contact.inactive", backgroundColor:"#ffffff"
+                attributeState "active", label:'contact', icon:"st.contact.contact.active", backgroundColor:"#53a7c0"
             }
         }
 
@@ -44,11 +44,11 @@ def updateStatus(Map status) {
     log.debug "updateStatus => '${status}'"
     // need to convert open to active and closed to inactive
 
-    def newState = status.tripped.current ? "active" : "inactive"
+    def newState = status.tripped.current ? "open" : "closed"
 
-    def desc = status.tripped.current ? "Detected Motion" : "Motion Has Stopped"
+    def desc = status.tripped.current ? "Contact Open" : "Contact Closed"
 
-    sendEvent (name: "motion", value: "${newState}", descriptionText: "${desc}")
-
+    sendEvent(name: "contact", value: "${newState}", descriptionText: "${desc}")
+    
    	sendEvent(name: "battery", value: status.battery.level, display: true, displayed: true)
 }
